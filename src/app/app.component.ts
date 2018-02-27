a52856a07973608180d954c043a810f23ee8ae2c
Stas Mamontov <smamontov@smamontov.techinfocom.com>
2018-02-27 19:40:31 +0500

    add pomodoro timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,53 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-    selector: 'hello-angular',
-    template: '<h1>{{greeting}}</h1>'
+  selector: 'pomodoro-timer',
+  template: `<div class="text-center">
+              <img src="img/tamato.png" width=20% alt="Pomodoro">
+              <h1>{{ minutes }}:{{ seconds | number: '2.0' }}</h1>
+              <p>
+                <button (click)="togglePause()" class="btn btn-danger">{{ buttonLabel }}</button>
+              </p>
+            </div>`
 })
 
 export class AppComponent {
-    greeting: string;
-    constructor() {
-        this.greeting = 'Hello Angular 2!';
+  fixMin: number = 24;
+  fixSec: number = 59;
+  minutes: number;
+  seconds: number;
+  isPaused: boolean;
+  buttonLabel: string;
+
+  constructor() {
+    this.resetTimer();
+    setInterval(() => this.tick(), 1000);
+  }
+
+  private resetTimer(): void {
+    this.minutes = this.fixMin;
+    this.seconds = this.fixSec;
+    this.buttonLabel = 'Start';
+    this.togglePause();
+  }
+
+  private tick(): void {
+    if (!this.isPaused) {
+      this.buttonLabel = 'Pause';
+      if (--this.seconds < 0) {
+        this.seconds = this.fixSec;
+        if (--this.minutes < 0) {
+          this.resetTimer();
+        }
+      }
     }
+  }
+
+  private togglePause(): void {
+    this.isPaused = !this.isPaused;
+
+    if (this.minutes < this.fixMin || this.seconds < this.fixSec) {
+      this.buttonLabel = this.isPaused ? 'Resume' : 'Pause';
+    }
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,6 @@ module.exports = {
             name: ['app', 'polyfills']
         }),
         // минифицирует код сборок
-        //new UglifyJSPlugin() 
+        // new UglifyJSPlugin() 
   ]
 }
